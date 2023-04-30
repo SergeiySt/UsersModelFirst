4dca8aad22a1672b54b5db6651607ce3429765d0
SergeiySt <ser.st.1989@gmail.com>
2023-04-30 18:14:01 +0300

    add_4

---
+++ WpfApp14/RegistrationUsers.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp14.Model;
+using WpfApp14.Views;
+using static WpfApp14.Views.DataPage;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Interaction logic for RegistrationUsers.xaml
+    /// </summary>
+    public partial class RegistrationUsers : Window
+    {
+
+        public RegistrationUsers()
+        {
+            InitializeComponent();
+            comboboxRole.ItemsSource = AppData.db.Roles.ToList();
+        }
+
+        public event EventHandler UserRegistered;
+
+        private void OnUserRegistered()
+        {
+            UserRegistered?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = login.Text;
+            string Password = password2.Text;
+            int roleId = (int)comboboxRole.SelectedValue;
+
+            if(AppData.db.Users.Any(x=> x.Login == login.Text))
+            {
+                MessageBox.Show("Такий логін вже існує.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Users users = new Users
+            {
+                RolesId = roleId,
+                Login = login.Text,
+                Password = password2.Text
+            };
+
+            using (var db = new Financial_conditionEntities())
+            {
+                db.Users.Add(users);
+                db.SaveChanges();
+            }
+            login.Text = " ";
+            password2.Text = " ";
+
+            OnUserRegistered();
+        }
+    }
+}
